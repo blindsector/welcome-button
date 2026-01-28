93c98019ce1db76612d0a3ec9c598c86c6e08bf8
blindsector <blind_sector@protonmail.com>
2026-01-28 18:53:41 +0100

    Update semanticEngine.js

--- semanticEngine.js
+++ semanticEngine.js
@@ -1,20 +1,15 @@
-import { nounMap, verbMap } from "./semanticExtras.js"
-
-const reverse = obj => Object.fromEntries(Object.entries(obj).map(([k,v]) => [v,k]))
-
-const reverseNouns = reverse(nounMap)
-const reverseVerbs = reverse(verbMap)
-
-function transform(word, map) {
-  const clean = word.toLowerCase().replace(/[.,!?]/g, "")
-  const punct = word.match(/[.,!?]$/)?.[0] || ""
-  return (map[clean] || word) + punct
-}
-
 export function encodeText(text) {
-  return text.split(" ").map(w => transform(transform(w, nounMap), verbMap)).join(" ")
+    return text
+        .replace(/пари/gi, "капачки")
+        .replace(/оръжие/gi, "гърмялка")
+        .replace(/среща/gi, "засечка")
+        .replace(/полиция/gi, "сиви шапки");
 }
 
 export function decodeText(text) {
-  return text.split(" ").map(w => transform(transform(w, reverseNouns), reverseVerbs)).join(" ")
+    return text
+        .replace(/капачки/gi, "пари")
+        .replace(/гърмялка/gi, "оръжие")
+        .replace(/засечка/gi, "среща")
+        .replace(/сиви шапки/gi, "полиция");
 }
