d876f103fe0b4c6c3c3c146faf563509fa5d6046
blindsector <blind_sector@protonmail.com>
2026-01-28 10:56:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function encodeText() {
-    alert("Работи!");
+    let text = document.getElementById("inputText").value.toLowerCase();
+
+    let dictionary = {
+        "здравей": "здрастио",
+        "как": "кво",
+        "си": "сио",
+        "приятел": "брато",
+        "работа": "мъка",
+        "пари": "кинти",
+        "шеф": "главатар",
+        "проблем": "драма",
+        "кола": "бричка",
+        "жена": "шефица",
+        "мъж": "пич",
+        "храна": "манджа",
+        "къща": "бърлога",
+        "отивам": "изчезвам",
+        "ела": "допълзи",
+        "чао": "бегай",
+        "добре": "екстра",
+        "лошо": "злеяко"
+    };
+
+    let words = text.split(" ");
+    let translated = words.map(word => dictionary[word] || word);
+
+    document.getElementById("outputText").value = translated.join(" ");
 }
